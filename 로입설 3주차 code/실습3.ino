dd4da719b13becece8b6f182181cd5ce4b1db5b0
Limohate <asked3425@hanyang.ac.kr>
2024-03-22 15:03:34 +0900

    실습3 수정

--- 로입설 3주차 code/실습3.ino
+++ 로입설 3주차 code/실습3.ino
@@ -1,9 +1,7 @@
 #define PWM_L 10 //M1 
 #define DIR_L1 8
 #define DIR_L2 12
-#define PWM_R 9 //M2 
-#define DIR_R1 6
-#define DIR_R2 7
+
 
 void setup() {
   analogWriteResolution(12);
@@ -11,9 +9,6 @@ void setup() {
   pinMode(PWM_L, OUTPUT);
   pinMode(DIR_L1, OUTPUT);
   pinMode(DIR_L2, OUTPUT);
-  pinMode(PWM_R, OUTPUT);
-  pinMode(DIR_R1, OUTPUT);
-  pinMode(DIR_R2, OUTPUT);
 }
 
 void loop() {
@@ -31,17 +26,11 @@ void motor_Control(int pwm_1, int m_time)
   analogWrite(PWM_L, pwm_1);
   digitalWrite(DIR_L1, LOW);
   digitalWrite(DIR_L2, HIGH);
-  analogWrite(PWM_R, pwm_1);
-  digitalWrite(DIR_R1, LOW);
-  digitalWrite(DIR_R2, HIGH);
  }
   else{
   analogWrite(PWM_L, abs(pwm_1));
   digitalWrite(DIR_L1, HIGH);
   digitalWrite(DIR_L2, LOW);
-  analogWrite(PWM_R, abs(pwm_1));
-  digitalWrite(DIR_R1, HIGH);
-  digitalWrite(DIR_R2, LOW);
  }
  delay(m_time);
 }
